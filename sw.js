44d00fa0a9768dd9f4a2a31911e91a46fb60104d
Nhoquin <franmfilho@gmail.com>
2026-09-09 11:28:06 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='plp-2026-v3';
+const ASSETS=['./','./index.html','./manifest.json','./assets/brand-bg.png','./assets/league-logo.png','./assets/app-icon.png','./assets/icon-192.png','./assets/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
