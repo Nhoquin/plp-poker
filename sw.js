a7a52548fd582574371917ee6d0046ea8471e37b
Nhoquin <franmfilho@gmail.com>
2026-09-15 23:21:18 -0300

    Bump PLP cache to V27

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='plp-2026-v26';
+const CACHE='plp-2026-v27';
 const PAGE='./index.html';
 
-// V26: uma nova versão fica aguardando até o usuário aplicar ou todos os clientes
+// V27: uma nova versão fica aguardando até o usuário aplicar ou todos os clientes
 // antigos serem fechados. Isto evita recarregar o aplicativo durante uma partida.
 self.addEventListener('install',event=>{
   event.waitUntil((async()=>{
