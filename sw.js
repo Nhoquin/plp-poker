30f90052dffd9ab82c54f3b60a07ec41fb748d6b
Nhoquin <franmfilho@gmail.com>
2026-09-20 10:57:55 -0300

    V36: bump cache for fixed global background

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='plp-2026-v35';
+const CACHE='plp-2026-v36';
 const PAGE='./index.html';
 
-// V35: uma nova versão fica aguardando até o usuário aplicar ou todos os clientes
+// V36: uma nova versão fica aguardando até o usuário aplicar ou todos os clientes
 // antigos serem fechados. Isto evita recarregar o aplicativo durante uma partida.
 self.addEventListener('install',event=>{
   event.waitUntil((async()=>{
