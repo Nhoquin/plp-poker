c6ea5ed07d516c5abcd54a161e0d59f1d88e0d98
Nhoquin <franmfilho@gmail.com>
2026-09-20 11:18:50 -0300

    V38: bump cache for approved logo

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='plp-2026-v37';
+const CACHE='plp-2026-v38';
 const PAGE='./index.html';
 
-// V37: uma nova versão fica aguardando até o usuário aplicar ou todos os clientes
+// V38: uma nova versão fica aguardando até o usuário aplicar ou todos os clientes
 // antigos serem fechados. Isto evita recarregar o aplicativo durante uma partida.
 self.addEventListener('install',event=>{
   event.waitUntil((async()=>{
