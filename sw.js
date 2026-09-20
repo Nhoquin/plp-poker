788237c5069d72f2e1f84524bb9c1216921e83a0
Nhoquin <franmfilho@gmail.com>
2026-09-20 09:16:21 -0300

    V34: bump PWA cache for simplified home

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='plp-2026-v33';
+const CACHE='plp-2026-v34';
 const PAGE='./index.html';
 
-// V33: uma nova versão fica aguardando até o usuário aplicar ou todos os clientes
+// V34: uma nova versão fica aguardando até o usuário aplicar ou todos os clientes
 // antigos serem fechados. Isto evita recarregar o aplicativo durante uma partida.
 self.addEventListener('install',event=>{
   event.waitUntil((async()=>{
