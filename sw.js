c4d20316d8945b80ea31567513c26eba49bf4f58
Nhoquin <franmfilho@gmail.com>
2026-09-20 11:12:28 -0300

    V37: bump cache for fixed league logo

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='plp-2026-v36';
+const CACHE='plp-2026-v37';
 const PAGE='./index.html';
 
-// V36: uma nova versão fica aguardando até o usuário aplicar ou todos os clientes
+// V37: uma nova versão fica aguardando até o usuário aplicar ou todos os clientes
 // antigos serem fechados. Isto evita recarregar o aplicativo durante uma partida.
 self.addEventListener('install',event=>{
   event.waitUntil((async()=>{
